f929e12bbb262e8069679cbaa7c031852b74098f
Andrej <75538726+AndrejBuraz@users.noreply.github.com>
2023-03-22 09:28:24 +0100

    Add files via upload

---
+++ txtEditor/txtEditor/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace txtEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sadrzaj = "", putanja ="";
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    putanja = openFileDialog.FileName;
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        sadrzaj = reader.ReadToEnd();
+                    }
+                }
+            }
+            textBox1.Text = sadrzaj;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamWriter sw = new StreamWriter(openFileDialog.FileName))
+                    {
+                        sw.WriteLine(textBox1.Text);
+                    }
+
+                }
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string putanja = Directory.GetCurrentDirectory();
+            StreamWriter sw = File.CreateText(putanja);
+ 
+
+        }
+    }
+}
